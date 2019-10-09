dc084d94410cc2ccc986d666dae23e7454f436b4
mac <mac@zldemacbook-pro.local>
2019-10-09 10:52:34 +0800

    第二次

--- sortViewDemo/sortViewDemo/ViewController.m
+++ sortViewDemo/sortViewDemo/ViewController.m
@@ -8,20 +8,55 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController () <UITableViewDataSource,UITableViewDelegate>
+@property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, strong) NSMutableArray *datasArr;
 @end
 
+static NSString * tableCell = @"cell";
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    NSLog(@"创建git项目");
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.tableView.frame = CGRectMake(0, 0, 300, 300);
     
     
 }
 
+#pragma mark -----UITableView-----
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return self.datasArr.count;
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableCell];
+    
+    return cell;
+    
+}
+
+- (UITableView *)tableView {
+    
+    if (!_tableView) {
+        _tableView = [[UITableView alloc]init];
+    }
+    return _tableView;
+    
+}
+
+- (NSMutableArray *)datasArr {
+    
+    if (!_datasArr) {
+        _datasArr = [NSMutableArray array];
+    }
+    return _datasArr;
+    
+}
 
 @end
